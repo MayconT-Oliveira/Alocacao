4237a9c9ad2feee6b89e6be33c3ff149739344a0
unknown <adminn@LAB3-22.bglab.local>
2023-04-18 22:36:06 -0300

    v3

--- Alocacao/src/test/java/br/gov/sp/etec/alocacao/controller/CarroContollerTest.java
+++ Alocacao/src/test/java/br/gov/sp/etec/alocacao/controller/CarroContollerTest.java
@@ -1,17 +1,23 @@
 package br.gov.sp.etec.alocacao.controller;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import br.gov.sp.etec.alocacao.model.Carro;
+import br.gov.sp.etec.alocacao.repository.CarroRepository;
 
 @ExtendWith(SpringExtension.class)
 public class CarroContollerTest {
@@ -19,11 +25,62 @@ public class CarroContollerTest {
 	@InjectMocks
 	CarroController controller;
 	
+	@Mock
+	CarroRepository repository;
+	
+	@Mock
+	List<Carro> listaCarro;
+	
+	@Mock
+	Carro carro;
+	
 	@Test
-	public void carrosTeste() {
-		 List<Carro> carros = controller.carros();
-		 assertNotNull(carros);
+	public void carrosTesteComListaDeCarro() {
+		when(repository.findAll()).thenReturn(listaCarro);
+		List<Carro> carros = controller.carros();
+		assertNotNull(carros);
 	}
 	
-
+	@Test
+	public void carrosTesteNull() {
+		when(repository.findAll()).thenReturn(null);
+		List<Carro> carros = controller.carros();
+		assertNull(carros);
+	}
+	
+	@Test
+	public void adicionarCarrosTesteComSucesso() {
+		when(repository.save(any(Carro.class))).thenReturn(getCarro());
+		Carro c = controller.addCarro(getCarro());
+		assertEquals("WBM", c.getModelo());
+	}
+	
+	@Test
+	public void atualizarCarrosTesteComSucesso() {
+		when(repository.save(any(Carro.class))).thenReturn(getCarro());
+		Carro c = controller.attCarro(new Carro("Preto Claro","OPA-6924",687561l,"WBM"));
+		assertEquals(1l, c.getId());
+		assertEquals("WBM", c.getModelo());
+		assertEquals("Preto Claro", c.getCor());
+		assertEquals("OPA-6924", c.getPlaca());
+		assertEquals(687561l, c.getChassi());
+	}
+	
+	@Test
+	public void apagarCarrosTesteComSucesso() {
+		 controller.delCarro(1L);
+		 assertNull(null);
+	}
+	
+	private Carro getCarro() {
+		Carro carro = new Carro();
+		carro.setId(1l);
+		carro.setModelo("WBM");
+		carro.setCor("Preto Claro");
+		carro.setPlaca("OPA-6924");
+		carro.setChassi(687561l);
+		return carro;
+	}
 }
+	
+
